e634717237edced89d3c95eb6f224cdecba37ac0
Isiah Castro <isiahmcastro@smccme.edu>
2023-01-24 14:52:52 -0500

    Initial commit

---
+++ src/main/java/NOAAWeatherApp.java
@@ -0,0 +1,21 @@
+/**
+ * REVISION HISTORY
+ */
+public class NOAAWeatherApp {
+
+    public void menu() {
+
+    }
+    public void run() {
+        menu();
+    }
+
+    /**
+     *
+     * @param args filepath?
+     */
+    public static void main(String[] args){
+        NOAAWeatherApp driver = new NOAAWeatherApp();
+        driver.run();
+    }
+}
